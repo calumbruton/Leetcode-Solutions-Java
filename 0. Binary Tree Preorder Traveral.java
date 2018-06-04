6acc48de39183696b04e49c0a05871dd92e62bf6
Calum Bruton <calumbruton@Calums-MacBook-Pro.local>
2018-06-04 13:35:25 -0400

    BT Traversals

---
+++ 0. Binary Tree Preorder Traveral.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+            Deque<TreeNode> s = new ArrayDeque<>();
+            List<Integer> res = new ArrayList<>();
+            while (true) {
+                if (root!=null) {
+                    s.push(root);
+                    res.add(root.val);
+                    root=root.left;
+                } else {
+                    if (s.isEmpty()) return res;
+                    root = s.pop();
+                    root=root.right;
+                }
+            }
+        }
+    }
