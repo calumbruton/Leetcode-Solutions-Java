c0495eaacc4cc539fea6f9e72b41b83cd9b5ea4f
Calum Bruton <calumbruton@gmail.com>
2018-06-12 20:04:44 -0400

    Backtracking

---
+++ 052. N-Queens II.java
@@ -0,0 +1,38 @@
+class Solution {
+    private Set<Integer> col = new HashSet<Integer>();
+    private Set<Integer> diag1 = new HashSet<Integer>();
+    private Set<Integer> diag2 = new HashSet<Integer>();
+    int res;
+    
+    public int totalNQueens(int n) {
+        res = 0;
+        dfs(new ArrayList<String>(), 0, n);
+        return res;
+    }
+    private void dfs(List<String> list, int row, int n){
+        if (row == n){
+            this.res++;
+            return;
+        }
+        for (int i = 0; i < n; i++){
+            if (col.contains(i) || diag1.contains(row + i) || diag2.contains(row - i)) continue;
+            
+            char[] charArray = new char[n];
+            Arrays.fill(charArray, '.');
+            charArray[i] = 'Q';
+            String rowString = new String(charArray);
+            
+            list.add(rowString);
+            col.add(i);
+            diag1.add(row + i);
+            diag2.add(row - i);
+            
+            dfs(list, row + 1, n);
+            
+            list.remove(list.size() - 1);
+            col.remove(i);
+            diag1.remove(row + i);
+            diag2.remove(row - i);
+        }
+    }
+}
