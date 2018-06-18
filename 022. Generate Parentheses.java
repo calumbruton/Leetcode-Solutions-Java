a53fa7906ba0314ef3dd39fa23b2bb9ab59869d0
Calum Bruton <calumbruton@gmail.com>
2018-06-18 16:56:37 -0400

    backtracking

---
+++ 022. Generate Parentheses.java
@@ -0,0 +1,38 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        //Use backtracking
+        ArrayList <String> ans = new ArrayList<String>();
+        backtrack(n, n*2, ans, "", 0);
+        return ans;
+    }
+    
+    public void backtrack(int n, int x, List<String> ans, String bracks, int open){
+        //System.out.println(x + ": " + bracks);
+        if (x==0){
+            //System.out.println("yup: " + open);
+            if (open == 0)
+                ans.add(bracks);
+            return;
+        }
+        //try a closing bracket
+        if(open > 0){
+            x--;
+            open--;
+            bracks+=")";
+            backtrack(n, x, ans, bracks, open);
+            bracks = bracks.substring(0,bracks.length()-1);
+            x++;
+            open++;
+        }
+        //try an opening bracket
+        if(open < x){
+            x--;
+            open++;
+            bracks+="(";
+            backtrack(n, x, ans, bracks, open);
+            bracks = bracks.substring(0,bracks.length()-1);
+            x++;
+            open--; 
+        }
+    }
+}
