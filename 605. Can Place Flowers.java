c96a5fbad3c9a6a3c28236949fce4987db9c5a53
Calum <cbruton@wavecomp.com>
2019-06-02 12:55:18 -0700

    Greedy Array

---
+++ 605. Can Place Flowers.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int i = 0;
+        while (i < flowerbed.length) {
+            if (flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) && (i == flowerbed.length - 1 || flowerbed[i + 1] == 0)) {
+                flowerbed[i] = 1;
+                n--;
+            }
+            i++;
+        }
+        return 0 >= n;
+    }
+}
